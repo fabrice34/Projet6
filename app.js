ec880737bb7969bc6e79f674dd32e3f86a808a90
fabrice34 <labarrefabrice6@gmail.com>
2020-11-14 13:59:14 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://user_34:projet_6@cluster0.5vj6g.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,             /*logique pour se connecter à mongodb*/
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use((req, res, next) => {                /* méthode permettant d'empêcher des erreurs CORS */
+    res.setHeader('Access-Control-Allow-Origin', '*');  
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+  
+app.use(bodyParser.json());   /*Requêtes exploitables*/
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/stuff', stuffRoutes);   /* les routes  */
+app.use('/api/auth', userRoutes);
+module.exports = app;
